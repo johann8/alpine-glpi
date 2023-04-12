2b55dfd768ba5d8eec481780112a369b8994d1ed
johann8 <jo.hahn@gmx.de>
2023-04-12 11:43:16 +0200

    added more verbosity

--- rootfs/bin/docker-entrypoint.sh
+++ rootfs/bin/docker-entrypoint.sh
@@ -68,6 +68,12 @@ SetPermissions () {
 
 }
 
+echo "+----------------------------------------------------------+"
+echo "|                                                          |"
+echo "|      Welcome to GLPI - IT Asset Management Docker!       |"
+echo "|                                                          |"
+echo "+----------------------------------------------------------+"
+echo
 echo "Starting startup scripts in /docker-entrypoint-init.d ..."
 
 for script in $(find /docker-entrypoint-init.d/ -executable -type f); do
@@ -93,39 +99,51 @@ SetPermissions
 # Delete installation file "install.php" if glpi is already installed
 if [[ -e "/var/www/glpi/config/glpicrypt.key" ]]
 then
-   echo "GLPI has already been installed."
+   echo "+-----------------------------------------------+"
+   echo "|       GLPI has already been installed!        |"
+   echo "+-----------------------------------------------+"
+   echo
+   #echo "GLPI has already been installed."
    echo "GLPI installation \"install.php\" file will be deleted."
-   echo -n "Deleting \"install.php\" file... "
+   echo -n "Deleting \"install.php\" file...               "
    rm -rf /var/www/glpi/install/install.php
    echo "[done]"
 
    # set www root directory
-   echo -n "Setting www root directory...    "
+   echo -n "Setting www root directory...                  "
    sed -i -e "s+###WWW_ROOT_DIRECTORY###+${WEB_ROOT_PATH}+" /etc/nginx/nginx.conf
    echo "[done]"
 else
-   echo "GLPI is not installed yet."
+   echo "+-----------------------------------------------+"
+   echo "|          GLPI is not installed yet!           |"
+   echo "+-----------------------------------------------+"
+   echo
+   #echo "GLPI is not installed yet."
 
    # set www install root directory
-   echo -n "Setting www install root directory... "
+   echo -n "Setting www install root directory...          "
    sed -i -e "s+###WWW_ROOT_DIRECTORY###+${INSTALL_WEB_ROOT_PATH}+" /etc/nginx/nginx.conf
    echo "[done]"
 fi
 
 # Set options into custom.ini
-echo -n "Setting \"date.timezone\" into custom.ini..."
+echo "+-----------------------------------------------+"
+echo "|   Customizing from environment variables...   |"
+echo "+-----------------------------------------------+"
+echo
+echo -n "Setting \"date.timezone\" into custom.ini...       "
 sed -i -e '/date.timezone=/c\date.timezone="'${TZ}'"' /etc/php81/conf.d/custom.ini
 echo "[done]"
 
-echo -n "Setting \"date.timezone\" into php.ini..."
+echo -n "Setting \"date.timezone\" into php.ini...          "
 sed -i -e '/;date.timezone =/c\date.timezone = '${TZ}'' /etc/php81/php.ini
 echo "[done]"
 
-echo -n "Setting \"upload_max_filesize\" into custom.ini..."
+echo -n "Setting \"upload_max_filesize\" into custom.ini... "
 sed -i -e '/upload_max_filesize= /c\upload_max_filesize= '${UPLOAD_MAX_FILESIZE}'' /etc/php81/conf.d/custom.ini
 echo "[done]"
 
-echo -n "Setting \"post_max_size\" into custom.ini..."
+echo -n "Setting \"post_max_size\" into custom.ini...       "
 sed -i -e '/post_max_size= /c\post_max_size= '${POST_MAX_SIZE}'' /etc/php81/conf.d/custom.ini
 echo "[done]"
 # ========== END ==========
