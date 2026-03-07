44aab3442277f4081bef72383b7e5755a3055ae7
johann8 <jo.hahn@gmx.de>
2026-03-07 12:58:56 +0100

    small changes

--- build_image.sh
+++ build_image.sh
@@ -1,32 +1,48 @@
 #!/bin/bash
 
 # set variables
-_VERSION=10.0.24
+D_IMAGE_VERSION=10.0.24.1
 _TAG=alpine-glpi
+BASE_IMAGE=alpine:3.22
+GLPI_VERSION=10.0.24
+PHP_VERSION=83
+#ALPINE_VERSION=3.23
 
-# build image glpi
-docker build -t johann8/${_TAG}:${_VERSION} . 2>&1 | tee ./build.log
+
+# build glpi docker
+# docker build -t johann8/${_TAG}:${D_IMAGE_VERSION} . 2>&1 | tee ./build.log
+docker build \
+  --build-arg=BASE_IMAGE=${BASE_IMAGE} \
+  --build-arg=GLPI_VERSION=${GLPI_VERSION} \
+  --build-arg=PHP_VERSION=${PHP_VERSION} \
+  --platform=linux/amd64 \
+  --tag=johann8/${_TAG}:${D_IMAGE_VERSION} \
+  --file=./Dockerfile . 2>&1 | tee ./build.log
+
+
 _BUILD=$?
+
+# Check
 if ! [ ${_BUILD} = 0 ]; then
    echo "ERROR: Docker Image build was not successful"
    exit 1
 else
    echo "Docker Image build successful"
    docker images -a
-   docker tag johann8/${_TAG}:${_VERSION} johann8/${_TAG}:latest
+   docker tag johann8/${_TAG}:${D_IMAGE_VERSION} johann8/${_TAG}:latest
 fi
 
 #push image to dockerhub
 if [ ${_BUILD} = 0 ]; then
    echo "Pushing docker images to dockerhub..."
-   docker push johann8/${_TAG}:${_VERSION}
+   docker push johann8/${_TAG}:${D_IMAGE_VERSION}
    docker push johann8/${_TAG}:latest
    _PUSH=$?
    docker images -a |grep glpi
 fi
 
 #
-### build docker contsiner image glpi crond
+### show
 #
 if [ ${_PUSH} = 0 ]; then
    echo "Pushing docker image \"${_TAG}\" was successfull."
@@ -34,10 +50,10 @@ else
    echo "ERROR: Pushing docker image \"${_TAG}\" was not successfull."
 fi
 
-#delete build
+# delete build docker image
 if [ ${_PUSH} = 0 ]; then
    echo "Deleting docker images..."
-   docker rmi johann8/${_TAG}:${_VERSION}
+   docker rmi johann8/${_TAG}:${D_IMAGE_VERSION}
    docker rmi johann8/${_TAG}:latest
    #docker rmi $(docker images -f "dangling=true" -q)
    docker images -a
