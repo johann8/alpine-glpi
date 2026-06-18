99ad7cc39694d0ffc65eaa7b0f33ed21f5d4acee
johann8 <jo.hahn@gmx.de>
2026-06-18 11:27:25 +0200

    changed var _TAG to D_IMAGE_TAG

--- build_image.sh
+++ build_image.sh
@@ -2,21 +2,19 @@
 
 # set variables
 D_IMAGE_VERSION=10.0.25
-_TAG=alpine-glpi
+D_IMAGE_TAG=alpine-glpi
 BASE_IMAGE=alpine:3.22
 GLPI_VERSION=10.0.25
 PHP_VERSION=83
-#ALPINE_VERSION=3.23
-
 
 # build glpi docker
-# docker build -t johann8/${_TAG}:${D_IMAGE_VERSION} . 2>&1 | tee ./build.log
+# docker build -t johann8/${D_IMAGE_TAG}:${D_IMAGE_VERSION} . 2>&1 | tee ./build.log
 docker build \
   --build-arg=BASE_IMAGE=${BASE_IMAGE} \
   --build-arg=GLPI_VERSION=${GLPI_VERSION} \
   --build-arg=PHP_VERSION=${PHP_VERSION} \
   --platform=linux/amd64 \
-  --tag=johann8/${_TAG}:${D_IMAGE_VERSION} \
+  --tag=johann8/${D_IMAGE_TAG}:${D_IMAGE_VERSION} \
   --file=./Dockerfile . 2>&1 | tee ./build.log
 
 
@@ -29,14 +27,14 @@ if ! [ ${_BUILD} = 0 ]; then
 else
    echo "Docker Image build successful"
    docker images -a
-   docker tag johann8/${_TAG}:${D_IMAGE_VERSION} johann8/${_TAG}:latest
+   docker tag johann8/${D_IMAGE_TAG}:${D_IMAGE_VERSION} johann8/${D_IMAGE_TAG}:latest
 fi
 
 #push image to dockerhub
 if [ ${_BUILD} = 0 ]; then
    echo "Pushing docker images to dockerhub..."
-   docker push johann8/${_TAG}:${D_IMAGE_VERSION}
-   docker push johann8/${_TAG}:latest
+   docker push johann8/${D_IMAGE_TAG}:${D_IMAGE_VERSION}
+   docker push johann8/${D_IMAGE_TAG}:latest
    _PUSH=$?
    docker images -a |grep glpi
 fi
@@ -45,16 +43,16 @@ fi
 ### show
 #
 if [ ${_PUSH} = 0 ]; then
-   echo "Pushing docker image \"${_TAG}\" was successfull."
+   echo "Pushing docker image \"${D_IMAGE_TAG}\" was successfull."
 else
-   echo "ERROR: Pushing docker image \"${_TAG}\" was not successfull."
+   echo "ERROR: Pushing docker image \"${D_IMAGE_TAG}\" was not successfull."
 fi
 
 # delete build docker image
 if [ ${_PUSH} = 0 ]; then
    echo "Deleting docker images..."
-   docker rmi johann8/${_TAG}:${D_IMAGE_VERSION}
-   docker rmi johann8/${_TAG}:latest
+   docker rmi johann8/${D_IMAGE_TAG}:${D_IMAGE_VERSION}
+   docker rmi johann8/${D_IMAGE_TAG}:latest
    #docker rmi $(docker images -f "dangling=true" -q)
    docker images -a
 fi
